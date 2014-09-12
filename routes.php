da02988a2da7aeffe5ca61311400af8c48807fda
unknown <ADMIN5@ADMIN5.ftis.unpar>
2014-09-12 12:20:48 +0700

    asd

--- routes.php
+++ routes.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
+testingtestingtestingtestingtestingtesting
+
+
+
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the Closure to execute when that URI is requested.
